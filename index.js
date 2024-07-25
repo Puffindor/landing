55275c752db0f6e6f9bb47e2b5acc4f035d0e8fb
Ilya Mzierov <ilmiz2012@gmail.com>
2024-07-25 17:55:53 +0700

    fixes

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const burger = document.querySelector('.burger');
 const mobileNav = document.querySelector('.mobile-nav__out');
 const closeMobileNav = document.querySelector('.cross');
 
+const closeNav = function () {
+ mobileNav.classList.remove('active');
+};
+
 burger.addEventListener('click', function () {
  if (!mobileNav.classList[1]) mobileNav.classList.add('active');
 });
 
-closeMobileNav.addEventListener('click', function () {
- mobileNav.classList.remove('active');
-});
+closeMobileNav.addEventListener('click', closeNav);
 
 const scrollTo = function () {
  document
   .querySelector(`.${this.dataset.element}`)
   .scrollIntoView({ behavior: 'smooth' });
+ closeNav();
 };
 
 for (let el of nav) {
