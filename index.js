9a59f251a96b9875ecd063788486d5040a7640dd
Ilya Mzierov <ilmiz2012@gmail.com>
2024-07-25 19:38:27 +0700

    много фиксов

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ const nav = document.querySelectorAll('.nav');
 const navFooter = document.querySelectorAll('.nav-f');
 const burger = document.querySelector('.burger');
 const mobileNav = document.querySelector('.mobile-nav__out');
-const closeMobileNav = document.querySelector('.cross');
+const closeMobileNav = document.querySelectorAll('.cross');
+
+const speakersOrangeBtn = document.querySelector('.speakers__btns__b2');
+const speakersBlueBtn = document.querySelector('.speakers__btns__b1');
+const popupOut = document.querySelector('.popup-out');
+const orangePopup = document.querySelector('.popup-in__orange');
+const bluePopup = document.querySelector('.popup-in__blue');
+
+speakersOrangeBtn.addEventListener('click', openOrangePopup);
+speakersBlueBtn.addEventListener('click', openBluePopup);
+
+function openOrangePopup() {
+ openPopup();
+ orangePopup.classList.add('active');
+}
+
+function openBluePopup() {
+ openPopup();
+ bluePopup.classList.add('active');
+}
+
+function openPopup() {
+ popupOut.classList.add('active');
+}
 
 const closeNav = function () {
  mobileNav.classList.remove('active');
+ orangePopup.classList.remove('active');
+ bluePopup.classList.remove('active');
+ popupOut.classList.remove('active');
 };
 
 burger.addEventListener('click', function () {
  if (!mobileNav.classList[1]) mobileNav.classList.add('active');
 });
 
-closeMobileNav.addEventListener('click', closeNav);
+for (let el of closeMobileNav) {
+ el.addEventListener('click', closeNav);
+}
+
+// closeMobileNav.addEventListener('click', closeNav);
 
 const scrollTo = function () {
  document
